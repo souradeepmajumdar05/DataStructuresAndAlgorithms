776f628c13cd7a0a5fff2e6f04b26151dcdee542
souradeepmajumdar05 <souradeepmajumdar05 @gmail.com>
2019-04-08 02:40:53 +0530

    Dynamic Programming (first commit)

---
+++ leetcode.com/Dynamic Programming/MaxProfit.java
@@ -0,0 +1,22 @@
+package dp;
+
+public class MaxProfit 
+{
+	 public int maxProfit(int[] prices) 
+	    {
+	        if (prices.length == 0) 
+	            return 0;
+	        
+	        int min = prices[0];
+	        int max = 0;
+	        
+	        for (int i=1;i<prices.length;i++) 
+	        {
+	            max = Math.max(max, prices[i] - min);
+	            min = Math.min(min, prices[i]);
+	        }
+	        
+	        return max;        
+	    }
+	
+}
